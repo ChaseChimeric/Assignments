6b7dcadd3f4444f66840015771a8ea4a0fa0166f
Ryan Fong <ryfong704@gmail.com>
2020-09-06 01:07:03 +0000

    Reformatted module comments

--- a02p2.py
+++ a02p2.py
@@ -14,39 +14,58 @@ GALLONS_PER_FOOT = 0.00869565217391304347826086956522
 #Labor per foot is 8/115
 LABOR_PER_FOOT = 0.06956521739130434782608695652174
 
-#Multiplies square_feet and GALLONS_PER_FOOT to get number of gallons
+
+#Module Name:   calculate_paint_gallons_required
+#Parameters:    1 float, total_wall_space
+#Desription:    Multiplies square_feet and GALLONS_PER_FOOT to get number of gallons
 def calculate_paint_gallons_required(total_wall_space):
     return total_wall_space * GALLONS_PER_FOOT
 
-#Multiplies square_feet and LABOR_PER_FOOT to get number of hours
+
+#Module Name:   calculate_labor_hours_required
+#Parameters:    1 float, total_wall_space
+#Desription:    Multiplies square_feet and LABOR_PER_FOOT to get number of hours
 def calculate_labor_hours_required(total_wall_space):
     return total_wall_space * LABOR_PER_FOOT
 
-#Multiplies calculate_paint_gallons_required() and paint_price
+
+#Module Name:   calculate_cost_of_paint
+#Parameters:    2 floats, paint_gallons_required and paint_price_per_gallon
+#Desription:    Multiplies calculate_paint_gallons_required() and paint_price
 def calculate_cost_of_paint(paint_gallons_required, paint_price_per_gallon):
     return paint_gallons_required * paint_price_per_gallon
 
-#Multiplies calculate_labor_hours_required() and the LABOR_COST
+
+#Module Name:   calculate_cost_of_labor
+#Parameters:    2 floats, labor_hours_required and labor_cost_per_hour
+#Desription:    Multiplies calculate_labor_hours_required() and the LABOR_COST
 def calculate_cost_of_labor(labor_hours_required, labor_cost_per_hour):
     return labor_hours_required * labor_cost_per_hour
 
-#Adds calculate_cost_of_paint() and calculate_cost of labor together and returns that value
+
+#Module Name:   calculate_total_job_cost
+#Parameters:    2 floats, cost_of_paint and cost_of_labor
+#Desription:    Adds calculate_cost_of_paint() and calculate_cost of labor together and returns that value
 def calculate_total_job_cost(cost_of_paint, cost_of_labor):
     return cost_of_paint + cost_of_labor
 
+
+#Module Name:   main
+#Parameters:    None
+#Desription:    Gathers input from user, and calls all required modules and prints output
 def main():
 
-    #Gathers input from user
     print("Enter the square footage of the area to be painted: ")
     square_feet = float(input())
     print("Enter the price of paint (per gallon) to be used: ")
     paint_price = float(input())
 
-    #Calls all required modules and prints output
+
     print("Gallons of paint required:   ", calculate_paint_gallons_required(square_feet))
     print("Labor hours required:        ", calculate_labor_hours_required(square_feet))
     print("Cost of paint:               ", calculate_cost_of_paint(calculate_paint_gallons_required(square_feet), paint_price))
     print("Cost of labor:               ", calculate_cost_of_labor(calculate_labor_hours_required(square_feet), LABOR_COST))
     print("Total job cost is:           ", calculate_total_job_cost(calculate_cost_of_paint(calculate_paint_gallons_required(square_feet), paint_price), calculate_cost_of_labor(calculate_labor_hours_required(square_feet), LABOR_COST)))
+
 #Run Program
 main()
