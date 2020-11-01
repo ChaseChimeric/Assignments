53d1c5bd9a382a178f19b50748dea65d243feafc
ChaseChimeric <ryfong704@gmail.com>
2020-11-01 12:57:57 -0800

    asdfghjkl;'

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 """
-Program Name:   Golf Score Read
-Filename        main.py
-Author:         Ryan Fong
-Date:           31 October 2020
-Assignment:     Assignment 9 Part 1
-Description:    A program to read the golf scores and player names in a file.
-Sources:        \/
-                https://dbader.org/blog/python-file-io
-                https://pscustomobject.github.io/python/Python-Reset-Read-Write-Position/
+Program Name:
+Filename       main.py
+Author:        Ryan Fong
+Date:
+Assignment:
+Description:
+Sources:
 """
 
 
-def read_records_from_file_and_display(filename):
+def basic():
     """
-    Module Name:    read_records_from_file_and_display()
-    Parameters:     1 String 'filename'
-    Description:    Reads player names and scores from golf.dat and prints them
+    Module Name:
+    Parameters:
+    Description:
     """
-    with open(filename, 'r') as InFile:
-        LineCount = len(InFile.read().split('\n')) - 1  #Gets amount of lines with data in them
-        InFile.seek(0, 0)  #Resets read position of file
-        for line in range(0, LineCount):
-            LineData = InFile.readline().rstrip('\n').split('#')  #reads a line, strips '\n', and splits the line on deliminator '#'
-            print(LineData[0], 'had a score of: ', LineData[1])  #prints name and respective score
+
     return 0
 
 
@@ -32,7 +25,7 @@ def main():
     Parameters:    None
     Description:   Main module for code
     """
-    read_records_from_file_and_display('golf.dat')
+    basic()
 
 
 if __name__ == "__main__":
