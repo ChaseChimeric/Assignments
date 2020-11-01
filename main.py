0a061e647cb36541ec15657d5548b740a6420749
ChaseChimeric <ryfong704@gmail.com>
2020-10-31 19:55:12 -0700

    yeet

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def read_records_from_file_and_display(filename):
     Description:
     """
     with open(filename, 'r') as InFile:
-        LineCount = len(InFile.read().split('\n')) - 1
+        LineCount = len(InFile.read().split('\n')) - 1  #Gets amount of lines with data in them
         InFile.seek(0,0)  #Resets read position of file
         for line in range(0, LineCount):
-            LineData = InFile.readline().rstrip('\n').split('#')
-            print(LineData[0], 'had a score of: ', LineData[1])
+            LineData = InFile.readline().rstrip('\n').split('#')  #reads a line, strips '\n', and splits the line on deliminator '#'
+            print(LineData[0], 'had a score of: ', LineData[1])  #prints name and respective score
     return 0
 
 
