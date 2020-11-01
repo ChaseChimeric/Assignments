4f0faa3bcc0224d5db37edbdd10f5b1408488f6b
ChaseChimeric <ryfong704@gmail.com>
2020-10-31 19:16:23 -0700

    MOOOARR

---
+++ a09p1.py
@@ -0,0 +1,52 @@
+"""
+Program Name:   Golf Score Logger
+Filename        main.py
+Author:         Ryan Fong
+Date:           31 October 2020
+Assignment:     Assignment 9 Part 1
+Description:    A program to store the golf scores and player names in a file.
+Sources:        None
+"""
+
+
+def get_records_from_user_and_write_to_file(filename):
+    """
+    Module Name:
+    Parameters:
+    Description:
+    """
+    with open(filename, 'a') as OutputFile:
+        go_again = True
+        while go_again == True:
+            print('Please enter the player\'s name:')
+            name_temp = input()  #Puts player name into variable for now
+            print('Please enter ', name_temp, '\'s score:')
+            score_temp = input()  #Puts score in variable
+            OutputFile.write(name_temp + '#' + score_temp + '\n')  #Writes name and score into file and adds new line '#' is deliminator
+            print('Would you like to add another record (y/n)?')
+            user_input = input().lower()
+#---------------------------------------------------- Input Validation / Exit Script
+            correct_input = False
+            while correct_input == False:
+                if user_input != 'n' and user_input != 'y':
+                    print("Please enter Yes('y') or No('n')")
+                elif user_input == 'n':
+                    go_again = False
+                    correct_input = True
+                elif user_input == 'y':
+                    correct_input = True
+#-----------------------------------------------------
+    return 0
+
+
+def main():
+    """
+    Module Name:   main()
+    Parameters:    None
+    Description:   Main module for code
+    """
+    get_records_from_user_and_write_to_file('golfData.dat')
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 """
-Program Name:
-Filename       *.py
-Author:        Ryan Fong
-Date:
-Assignment:
-Description:
-Sources:
+Program Name:   Golf Score Logger
+Filename        main.py
+Author:         Ryan Fong
+Date:           31 October 2020
+Assignment:     Assignment 9 Part 1
+Description:    A program to store the golf scores and player names in a file.
+Sources:        None
 """
 
 
-def basic_function():
+def read_records_from_file_and_display(filename):
     """
     Module Name:
     Parameters:
     Description:
     """
-
+    with open(filename, 'r') as InputFile:
+        for line in enumerate(InputFile.readlines(), 0):
+            print(line)
+            LineData = InputFile.readline().rstrip('\n').split('#')
+            print(LineData[0], 'had a score of: ', LineData[1])
     return 0
 
 
@@ -25,8 +29,7 @@ def main():
     Parameters:    None
     Description:   Main module for code
     """
-
-    print("hey")
+    read_records_from_file_and_display('golfData.dat')
 
 
 if __name__ == "__main__":
